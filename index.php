2fd9d69d5dec134546bb2fb66d38ad076e547a35
NicoDalmas <ndalmas9@gmail.com>
2016-11-12 12:46:12 -0300

    12/11

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+
+	if (isset($_GET['par'])) {
+		if(file_exists("img/$_GET[par]")){
+		unlink("img/$_GET[par]");
+		}
+	}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Galería php</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-5 col-md-offset-3">
+		<div class="page-header text-center">
+		  <h1>Galería de imágenes <small>con PHP</small></h1>
+		</div>
+	</div>
+</div>		
+		
+		<div class=row>
+		<div class="col-md-5 col-md-offset-3">
+		<form enctype="multipart/form-data" method="post" action="upload.php" class="form-group">
+					<div class="form-group">
+						<p>Elige un archivo
+						<input type="file" name="user-file"></p>
+							<div class="helper-text">Elige una imágen jpg (máximo 600kb)
+							</div>
+						<button type="submit" class="btn btn-primary">Cargar</button>
+					</div>
+		</form>
+		</div>
+		</div>
+		<hr>
+		<div class="col-md-12">
+		<?php
+		$patron='%\.(gif|jpe?g|png)$%i';
+		$imgs = dir('img');
+		while (($img = $imgs->read()) !== false) {
+		
+			if ( preg_match($patron, $img ) === 1 ) {
+				echo "<a href='index.php?par=$img'> <img src='img/$img' class='img-thumbnail'></a>"; 
+			}
+		}
+		
+		?>
+		</div>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,27 @@
+<?php
+function validar($file){
+	if($file['user-file']['type'] !== 'image/jpeg' || $file['user-file']['type'] !== 'image/png') {
+		return false;
+	}
+	
+	if ($file['user-file']['size'] > 600000) {
+		return false;
+	}
+	return true;
+}
+
+if (validar($_FILES)) {
+	$path = 'img/';
+	$file = $path.basename($_FILES['user-file']['name']);
+	if(move_uploaded_file($_FILES['user-file']['tmp_name'], $file)){
+		//header('location: index.php');
+		echo "Funciona wachin";
+	}
+}else{
+	echo "error";
+}
+
+
+
+//var_dump($_FILES);
+?>
